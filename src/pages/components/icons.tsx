f4a8bf6778ea003522e1baf6a62a73c1f011313a
Kris <kris@kaperi.com>
2023-06-20 14:18:59 +0200

    new icons

--- src/pages/components/icons.tsx
+++ src/pages/components/icons.tsx
@@ -1,5 +1,12 @@
+import React from 'react';
+type IcTProps = {
+    icons: "checks" | "x" | "chevronUp" | "chevronDown" | "search" | "dots";
+    classNameCustom?: string;
+  };
+  
 
-function IcT({icons, classNameCustom}: {icons: string, classNameCustom?: string | undefined } ): JSX.Element | null {
+
+function IcT({icons, classNameCustom}: IcTProps): JSX.Element | null {
     if (icons === "checks") {
         return(
             <svg xmlns="http://www.w3.org/2000/svg" className={classNameCustom ?? undefined} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -15,6 +22,37 @@ function IcT({icons, classNameCustom}: {icons: string, classNameCustom?: string 
                 <path d="M18 6l-12 12"></path>
                 <path d="M6 6l12 12"></path>
             </svg >
+        )
+    } else if (icons === "chevronUp") {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" className={classNameCustom ?? undefined} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M6 15l6 -6l6 6"></path>
+            </svg>
+        )
+    } else if (icons === "chevronDown") {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" className={classNameCustom ?? undefined} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M6 9l6 6l6 -6"></path>
+            </svg>
+        )
+    } else if (icons === "search") {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" className={classNameCustom ?? undefined} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+                <path d="M21 21l-6 -6"></path>
+            </svg>
+        )
+    } else if (icons === "dots") {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" className={classNameCustom ?? undefined} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+            </svg>
         )
     }
 
