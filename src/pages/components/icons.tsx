33ed233f0ed145b9b0bbd6f46f80e050bf717767
Kris <kris@kaperi.com>
2023-06-21 11:08:46 +0200

    icons and replace settings.json v2

--- src/pages/components/icons.tsx
+++ src/pages/components/icons.tsx
@@ -47,7 +47,7 @@ function IcT({icons, classNameCustom}: IcTProps): JSX.Element | null {
         )
     } else if (icons === "dots") {
         return (
-            <svg xmlns="http://www.w3.org/2000/svg" className={classNameCustom ?? undefined} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" className={classNameCustom ?? undefined} width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
                 <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
